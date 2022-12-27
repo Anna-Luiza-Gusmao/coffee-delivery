b3d9b56d68210eece0f521e9cf8cd6af11b912f3
Anna Luiza Gusmão <luizasgusmao@hotmail.com>
2022-12-27 20:09:20 -0300

    AddAddressForm

--- src/components/FormAddress/index.tsx
+++ src/components/FormAddress/index.tsx
@@ -3,60 +3,58 @@ import { MapPinLine } from 'phosphor-react'
 
 export function FormAddress() {
     return (
-        <section>
-            <AddressForm>
-                <HeaderAddressForm>
-                    <MapPinLine color="#C47F17" size={24} />
-                    <div style={{display: 'flex', flexDirection: 'column', paddingLeft: '0.5rem'}}> 
-                        <Title>Endereço de Entrega</Title>
-                        <SubTitle>Informe o endereço onde deseja receber seu pedido</SubTitle>
-                    </div>
-                </HeaderAddressForm>
+        <AddressForm>
+            <HeaderAddressForm>
+                <MapPinLine color="#C47F17" size={24} />
+                <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: '0.5rem' }}>
+                    <Title>Endereço de Entrega</Title>
+                    <SubTitle>Informe o endereço onde deseja receber seu pedido</SubTitle>
+                </div>
+            </HeaderAddressForm>
 
-                <AllInputs>
-                    <SmallerInput 
+            <AllInputs>
+                <SmallerInput
+                    type="text"
+                    placeholder="CEP"
+                    required
+                />
+                <StreetInput
+                    type="text"
+                    placeholder="Rua"
+                    required
+                />
+                <div style={{ display: 'flex' }}>
+                    <SmallerInput
                         type="text"
-                        placeholder="CEP" 
+                        placeholder="Número"
                         required
                     />
-                    <StreetInput 
+                    <span style={{ paddingLeft: '0.75rem' }}></span>
+                    <InputComplement
                         type="text"
-                        placeholder="Rua" 
+                        placeholder="Complemento"
+                    />
+                </div>
+                <div style={{ display: 'flex' }}>
+                    <SmallerInput
+                        type="text"
+                        placeholder="Bairro"
                         required
                     />
-                    <div style={{display: 'flex'}}>
-                        <SmallerInput 
-                            type="text"
-                            placeholder="Número" 
-                            required
-                        />
-                        <span style={{paddingLeft: '0.75rem'}}></span>
-                        <InputComplement 
-                            type="text"
-                            placeholder="Complemento" 
-                        />
-                    </div>
-                    <div style={{display: 'flex'}}>
-                        <SmallerInput 
-                            type="text"
-                            placeholder="Bairro" 
-                            required
-                        />
-                        <span style={{paddingLeft: '0.75rem'}}></span>
-                        <CityInput 
-                            type="text"
-                            placeholder="Cidade" 
-                            required
-                        />
-                        <span style={{paddingLeft: '0.75rem'}}></span>
-                        <UFInput 
-                            type="text"
-                            placeholder="UF" 
-                            required
-                        />
-                    </div>
-                </AllInputs>
-            </AddressForm>
-        </section>
+                    <span style={{ paddingLeft: '0.75rem' }}></span>
+                    <CityInput
+                        type="text"
+                        placeholder="Cidade"
+                        required
+                    />
+                    <span style={{ paddingLeft: '0.75rem' }}></span>
+                    <UFInput
+                        type="text"
+                        placeholder="UF"
+                        required
+                    />
+                </div>
+            </AllInputs>
+        </AddressForm>
     )
 }
